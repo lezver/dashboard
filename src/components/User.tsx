4dfbf6c828b08548a9cc7ed7038305dd88675462
Vladislav <lezver1994@gmail.com>
2024-06-21 13:41:07 +0300

    t

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,7 +4,7 @@ import userJpg from "../../public/user.jpg";
 const User: React.FC = () => {
 	return (
 		<div className="aside__user">
-			<Image src={userJpg} width={42} height={42} alt="user" />
+			{/* <Image src={userJpg} width={42} height={42} alt="user" /> */}
 			<div>
 				<p>Evano</p>
 				<span>Project Manager</span>
